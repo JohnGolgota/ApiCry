f365fdc2f4c8f70d74aaccac318e69f98e8e9d58
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2023-10-26 12:56:45 -0500

    hta good

--- encode.php
+++ encode.php
@@ -4,7 +4,6 @@ header('Content-Type: application/json; charset=utf-8');
 require_once "vendor/autoload.php";
 require_once "src/clases/validate.php";
 $debug = [];
-
 $json_data = file_get_contents("php://input");
 $data = json_decode($json_data, true);
 // $debug["data"] = $data;
@@ -33,6 +32,15 @@ if (isset($_GET["new_client"]) && $_GET["new_client"] == "true") {
 	return;
 }
 if (isset($_GET["new_token"]) && $_GET["new_token"] == "true") {
+	$validated = $authorization->valid();
+	// $debug["valid"] = $validated;
+	if (!isset($validated) || !isset($validated["code"]) || $validated["code"] !== 200) {
+		$err["Unauthorized"] = "Bad getway";
+		http_response_code($validated["code"] ?? 501);
+		echo json_encode($err, JSON_UNESCAPED_UNICODE);
+		// echo json_encode($debug, JSON_UNESCAPED_UNICODE);
+		return;
+	}
 	$newToken = $authorization->newToken($data);
 	http_response_code($newToken["code"] ?? 500);
 	$debug["newToken"] = $newToken;
--- src/config/config.php
+++ src/config/config.php
@@ -66,6 +66,10 @@ function get_const_from_apache_env($arg = null) {
 	define('DB_HOST', getenv('DB_HOST'));
 	define('DB_USERNAME', getenv('DB_USERNAME'));
 	define('DB_PASSWORD', getenv('DB_PASSWORD'));
+	define('API_KEY', getenv('API_KEY'));
+	define('ALGORITHM', getenv('ALGORITHM'));
+	define('DB_NAME', getenv('DB_NAME'));
+	define('API_DB_TABLE', getenv('API_DB_TABLE'));
 }
 // const_from_env();
 // const_from_text();
